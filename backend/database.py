6834e89c403d88ebc83fade657174a512017c9c6
Trivedihet8725 <hettrivedi2187@gmail.com>
2026-06-03 14:03:32 +0530

    Hardcode MongoDB URL for Vercel/Render

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
 # For local development we'll default to localhost if no ENV var exists
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://custofycrm:HeT%40%23%2A%212110@cluster0.t38sjc4.mongodb.net/?appName=Cluster0")
 
 # Initialize Motor Client
 client = AsyncIOMotorClient(MONGO_URL)
